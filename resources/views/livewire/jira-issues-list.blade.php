2e040dbd7bb8ab2d319cf9155f8f1a35556b2b15
Lars Bo Rasmussen <lrasmussen@aio-it.dk>
2025-03-15 03:05:10 +0100

    jira features

--- resources/views/livewire/jira-issues-list.blade.php
+++ resources/views/livewire/jira-issues-list.blade.php
@@ -17,17 +17,8 @@
                     </svg>
                 </div>
             </div>
-            <!-- Favorites Filter -->
+            <!-- Filter Buttons -->
             <div class="flex gap-2">
-                <button
-                    wire:click="toggleFavoriteFilter"
-                    class="inline-flex items-center px-3 py-2 border {{ $showFavoritesOnly ? 'border-yellow-400 bg-yellow-50 text-yellow-700' : 'border-gray-300 bg-white text-gray-700' }} rounded-md text-sm font-medium hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:bg-gray-700 dark:border-gray-600 dark:text-gray-200"
-                >
-                    <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 {{ $showFavoritesOnly ? 'text-yellow-400' : 'text-gray-400' }} mr-2" viewBox="0 0 20 20" fill="currentColor">
-                        <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
-                    </svg>
-                    {{ $showFavoritesOnly ? 'All Issues' : 'Favorites Only' }}
-                </button>
                 <div class="flex rounded-md shadow-sm" role="group">
                     <button
                         wire:click="toggleMyIssues"
