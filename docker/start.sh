bf337a6b350fe88b98aaa986055bc4e0dc1c0d40
Lars Bo Rasmussen <lrasmussen@aio-it.dk>
2025-03-15 12:25:53 +0100

    fix

--- docker/start.sh
+++ docker/start.sh
@@ -4,6 +4,12 @@
 if [ -z "$APP_KEY" ]; then
     php artisan key:generate --force
 fi
+mkdir -p /var/www/html/storage
+mkdir -p /var/www/html/storage/framework
+mkdir -p /var/www/html/storage/framework/cache
+mkdir -p /var/www/html/storage/framework/sessions
+mkdir -p /var/www/html/storage/framework/views
+mkdir -p /var/www/html/storage/logs
 chown -R www-data:www-data /var/www/html/storage
 php artisan optimize:clear
 php artisan migrate --force
