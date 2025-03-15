284369fb0c98a705dba622b07351979d9362da59
Lars Bo Rasmussen <lrasmussen@aio-it.dk>
2025-03-15 12:20:08 +0100

    fix docker

--- docker/start.sh
+++ docker/start.sh
@@ -4,6 +4,9 @@
 if [ -z "$APP_KEY" ]; then
     php artisan key:generate --force
 fi
+chown -R www-data:www-data /var/www/html/storage
+php artisan optimize:clear
+php artisan migrate --force
 
 # Start PHP-FPM
 php-fpm
